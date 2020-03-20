0f58d22fb29569b9e7396d328b7dec4831f7b07e
Ogenyi25 <johnogenyi4real@gmail.com>
2020-03-20 12:48:14 +0100

    updated the event and location section and also made them mobile responsive

--- src/screens/Event.jsx
+++ src/screens/Event.jsx
@@ -4,32 +4,37 @@ const Event = () => {
     return (
         <div className="events-page" id="event">
             <div className="events container-fluid row">
-                <div className="title">UPCOMING EVENTS</div>
-                    <div className="card1 col-md-3">
-                        <h3>Corper's SendForth</h3>
-                        <p>A sendforth service for our outgoing corp member(s).<br />
+                <div className="title">UPCOMING EVENTS
+                <h6 className="system">Hover on card to see more details</h6>
+            <h6 className="phone">Click on each card to see more details.<br />
+            Click outside the card to return it to default</h6>
+                </div>
+                <div className="card1 col-md-3">
+                    <h3>Corper's SendForth</h3>
+                    <p>A sendforth service for our outgoing corp member(s).<br />
                             A moment of togetherness, feasting and exhortation.
-                            <p className="time"><i className="fa fa-clock-o fa-lg" aria-hidden="true"></i> 6PM. <span><i className="fa fa-calendar" aria-hidden="true"></i> 12th March</span></p>     
-                        </p>
-                    </div>
+                            <p className="time"><i className="fa fa-clock-o fa-lg" aria-hidden="true"></i> 6PM. <span><i className="fa fa-calendar" aria-hidden="true"></i> 12th March</span></p>
+                    </p>
+                </div>
 
-                    <div className="card2 col-md-3">             
-                            <h3>National Easter Retreat</h3>
-                            <p>This is a period of partaking in the resulting blessings from christ manifestion at calvary. <br /></p>
-                            <h6 className="time"><span><i className="fa fa-calendar" aria-hidden="true"></i>12th - 15th April.</span></h6>
-                    </div>
+                <div className="card2 col-md-3">
+                    <h3>National Easter Retreat</h3>
+                    <p>This is a period of partaking in the resulting blessings from christ manifestion at calvary. <br /></p>
+                    <h6 className="time"><span><i className="fa fa-calendar" aria-hidden="true"></i>12th - 15th April.</span></h6>
+                </div>
 
-                    <div className="card3 col-md-3">                           
-                            <h3>National Easter Retreat</h3>
-                            <p>This is a period of partaking in the resulting blessings from christ manifestion at calvary. <br /></p>
-                            <h6 className="time"> <span><i className="fa fa-calendar" aria-hidden="true"></i> 12th - 15th April.</span></h6>
-                            </div>
-                    <div className="footer">
-                    </div>
+                <div className="card3 col-md-3">
+                    <h3>National Easter Retreat</h3>
+                    <p>This is a period of partaking in the resulting blessings from christ manifestion at calvary. <br /></p>
+                    <h6 className="time"> <span><i className="fa fa-calendar" aria-hidden="true"></i> 12th - 15th April.</span></h6>
+                </div>
+                <div className="footer">
+                </div>
             </div>
+            
         </div>
     )
 }
-                
-                
+
+
 export default Event;
--- src/screens/Location.jsx
+++ src/screens/Location.jsx
@@ -6,7 +6,11 @@ const Location = () => {
         <div className="location-page" id="location">
             <div className="title">
                 <img className="rotate" src={require("../assets/images/logo.png")} alt="" />
-                Other DLCF Locations</div>
+                Other DLCF Locations
+            </div>
+            <h6 className="system">Hover on card to see more details</h6>
+            <h6 className="phone">Click on each card to see more details.
+            Click outside the card to return it to default</h6>
             <div className="locations row container-fluid">
                 <div className="corpers col-md-4">
                     <div className="card">
@@ -39,9 +43,9 @@ const Location = () => {
                             <h2>DLCF<br /><span>City-College <br /><h4>Victory Kiddies Academy</h4></span></h2>
                         </div>
                     </div>
-                    
+
                     <button type="button" className="btn btn-warning">View More</button>
-                
+
                 </div>
 
             </div>
